23733b79d00c4ab9c06e9cfa0302528d268441f9
Tamara <t.kovacevic98@gmail.com>
2021-05-31 13:01:05 +0200

    router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import  React from "react";
 import {LoginPage} from './pages/LoginPage.js'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App () {
     return (
         <div className="App">
-            Here is the Latest React version: <strong>{React.version}</strong>
-        <LoginPage/>
+            <Router>
+                <Route path='/login' exact={true} component={LoginPage}/>
+
+             </Router>
         </div>
     );
 }
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -9,16 +9,6 @@ export function LoginPage(){
                 </div>
                 <h1 className="h1">Search Results</h1>
                 <div className="books">
-                <table>
-                    <thead>
-                    <tr>
-                        <th className="title-col">Title</th>
-                        <th className="author-col">Author</th>
-                        <th className="year-col">Pub Year</th>
-                    </tr>
-                    </thead>
-                    <tbody></tbody>
-                </table>
             </div>
         </div>
     );
