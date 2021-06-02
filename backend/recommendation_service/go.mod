656cba263153c379c3f83f047f8f8b7c9a1aaa24
jovan-jelicki <jovanzte@gmail.com>
2021-06-02 18:44:23 +0200

    feat - Created connection with Neo4j graphdb

--- backend/recommendation_service/go.mod
+++ backend/recommendation_service/go.mod
@@ -3,12 +3,17 @@ module github.com/david-drvar/xws2021-nistagram/recommendation_service
 go 1.16
 
 replace github.com/david-drvar/xws2021-nistagram/recommendation_service => ./recommendation_service
+
 replace github.com/david-drvar/xws2021-nistagram/common => ./../common
 
 require (
 	github.com/david-drvar/xws2021-nistagram/common v0.0.1
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/text v0.3.6 // indirect
+    github.com/neo4j/neo4j-go-driver/v4 v4.3.0
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	gorm.io/gorm v1.21.10
 )
--- backend/recommendation_service/main.go
+++ backend/recommendation_service/main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"github.com/david-drvar/xws2021-nistagram/common"
-	"github.com/david-drvar/xws2021-nistagram/recommendation_service/util/setup"
 	"github.com/gorilla/mux"
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
+	"log"
 	"net/http"
 )
 
 func main(){
-	db := common.InitDatabase()
+/*	db := common.InitDatabase()
 	err := setup.FillDatabase(db)
 	if err != nil {
 		panic("Cannot setup database tables. Error message: " + err.Error())
+	}*/
+	result, err := helloWorld("bolt://localhost:7687", "neo4j", "root")
+	if err != nil {
+		panic(err)
 	}
+	log.Println(result)
 
 	r := mux.NewRouter()
 
@@ -24,4 +30,43 @@ func main(){
 
 	http.Handle("/", c.Handler(r))
 	http.ListenAndServe(":8005", c.Handler(r))
+}
+
+func helloWorld(uri, username, password string) (string, error) {
+	driver, err := neo4j.NewDriver(uri, neo4j.BasicAuth(username, password, ""))
+	if err != nil {
+		return "", err
+	}
+	defer driver.Close()
+
+	session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+	defer session.Close()
+
+	greeting, err := session.WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
+		result, err := transaction.Run(
+			"MATCH (n:Greeting) RETURN n.message + ' ' + id(n)", map[string]interface{}{})
+		if err != nil {
+			return nil, err
+		}
+		if result.Next()  {
+			return result.Record().Values[0], nil
+		}
+		result, err = transaction.Run(
+			"CREATE (a:Greeting) SET a.message = $message RETURN a.message + ', from node ' + id(a)",
+			map[string]interface{}{"message": "hello, world"})
+		if err != nil {
+			return nil, err
+		}
+
+		if result.Next() {
+			return result.Record().Values[0], nil
+		}
+
+		return nil, result.Err()
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return greeting.(string), nil
 }
