8c2da44ba6ee77e6bf08efe5b812ccde5098e83d
David Drvar <david.drvar.bogdanovic@gmail.com>
2021-06-07 13:34:49 +0200

    feat - search user results front improved

--- frontend/src/components/HomePage/ProfileForSug.js
+++ frontend/src/components/HomePage/ProfileForSug.js
@@ -2,14 +2,14 @@ import "../../style/profileSug.css";
 import ProfileIcon from "../ProfileComponent/ProfileIcon";
 
 function ProfileForSug(props) {
-    const { username, caption,  urlText, iconSize,captionSize, storyBorder,hideAccountName,image,} = props;
+    const { username, caption,  urlText, iconSize,captionSize, storyBorder,hideAccountName,image, firstName, lastName} = props;
 
     return (
         <div className="profile">
             <ProfileIcon iconSize={iconSize} storyBorder={storyBorder} image={image} />
             {('maja' || caption) && !hideAccountName && (
                 <div className="textContainer">
-                    <span className="accountName">Maja</span>
+                    <span className="accountName">{firstName} {lastName} @{username}</span>
                     <span className={`caption ${captionSize}`}>{caption}</span>
                 </div>
             )}
--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -1,6 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {Alert, Button, Dropdown, DropdownButton, FormControl, InputGroup, Card} from "react-bootstrap";
 import axios from "axios";
+import ProfileIcon from "../components/ProfileComponent/ProfileIcon";
+import ProfileForSug from "../components/HomePage/ProfileForSug";
+import {useHistory} from "react-router-dom";
 
 export default function Search() {
     // Declare a new state variable, which we'll call "count"
@@ -10,6 +13,8 @@ export default function Search() {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [searchResult, setSearchResult] = useState([]);
+    const [searchPlaceholder, setSearchPlaceholder] = useState("search value");
+    const history = useHistory()
 
 
     function searchByUser() {
@@ -73,27 +78,65 @@ export default function Search() {
 
     if (searchCategory === 'User' && searchResult.length > 0) {
         var userResults = searchResult.map((user, i) =>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" alt="user pic"/>
-                <Card.Body>
-                    <Card.Title>@{user.username} ({user.firstName} {user.lastName})</Card.Title>
-                    <Card.Text>
-                        {user.biography}
-                    </Card.Text>
-                    <Button variant="primary">Visit profile</Button>
-                </Card.Body>
-            </Card>
+            // <Card style={{ width: '18rem' }}>
+            //     <Card.Img variant="top" src="holder.js/100px180" alt="user pic"/>
+            //     <Card.Body>
+            //         <Card.Title>@{user.username} ({user.firstName} {user.lastName})</Card.Title>
+            //         <Card.Text>
+            //             {user.biography}
+            //         </Card.Text>
+            //         <Button variant="primary">Visit profile</Button>
+            //     </Card.Body>
+            // </Card>
+            <ProfileForSug username={user.username} firstName={user.firstName} lastName={user.lastName} caption={user.biography} urlText="Follow" iconSize="medium" captionSize="small"  storyBorder={true} />
         );
+    }
+
+    function handleSearchCategoryChange(event) {
+        setSearchCategory(event)
+        switch (event) {
+            case "Location" :
+                setSearchPlaceholder("location");
+                break;
+            case "Tag" :
+                setSearchPlaceholder("tag");
+                break;
+            case "User" :
+                setSearchPlaceholder("username");
+                break;
+        }
+    }
+
+    function goBack() {
+        history.push({ //todo what to send back?
+            pathname: '/home',
+            state: { user:"", follow:false }
+        })
     }
 
 
     return (
         <div  className="App">
-            <h1 style={{marginLeft : "30px"}}>Search</h1>
+            {/*<h1 style={{marginLeft : "30px"}}>Search</h1>*/}
             <br/>
+            <Button variant="outline-dark" style={{marginLeft : "30px"}} onClick={goBack} >←Back</Button>{' '}
+
+            <br/><br/>
             <div className="row" style={{marginLeft : "10px"}}>
                 <div className="col-sm-5 mb-2">
-                    <input name="input" className="form-control" placeholder={"search value"} value={input} onClick={(e) => setInputErr("")} onChange={(e) => setInput(e.target.value)}/>
+                    <DropdownButton onSelect={(e) => handleSearchCategoryChange(e) }
+                                    as={InputGroup.Append}
+                                    variant="outline-secondary"
+                                    title={searchCategory}
+                                    id="input-group-dropdown-2"
+                    >
+                        <Dropdown.Item eventKey={"Location"} >Location</Dropdown.Item>
+                        <Dropdown.Item eventKey={"Tag"} >Tag</Dropdown.Item>
+                        <Dropdown.Item eventKey={"User"} >User</Dropdown.Item>
+                    </DropdownButton>
+                </div>
+                <div className="col-sm-5 mb-2">
+                    <input name="input" className="form-control" placeholder={searchPlaceholder} value={input} onClick={(e) => setInputErr("")} onChange={(e) => setInput(e.target.value)}/>
                     {inputErr.length > 0 &&
                     <span className="text-danger">{inputErr}</span>}
                 </div>
@@ -105,24 +148,12 @@ export default function Search() {
                     {searchCategory === "User" &&
                     <input name="input" className="form-control" placeholder={"last name"} value={lastName} onClick={(e) => setInputErr("")} onChange={(e) => setLastName(e.target.value)}/>}
                 </div>
-                <div className="col-sm-5 mb-2">
-                    <DropdownButton onSelect={(e) => setSearchCategory(e) }
-                                    as={InputGroup.Append}
-                                    variant="outline-secondary"
-                                    title={searchCategory}
-                                    id="input-group-dropdown-2"
-                    >
-                        <Dropdown.Item eventKey={"Location"} >Location</Dropdown.Item>
-                        <Dropdown.Item eventKey={"Tag"} >Tag</Dropdown.Item>
-                        <Dropdown.Item eventKey={"User"} >User</Dropdown.Item>
-                    </DropdownButton>
-                </div>
+
                 <div className="col-sm-4">
                     <Button variant="primary" onClick={search}>Search</Button>{' '}
                 </div>
             </div>
             <br/><br/>
-            <h2 style={{marginLeft : "30px"}}>Results:</h2>
             {searchResult.length > 0 && searchCategory === 'User' &&
                 <ul style={{marginLeft : "30px"}}>
                     {userResults}
