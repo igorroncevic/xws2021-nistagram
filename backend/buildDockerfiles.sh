5dbd21502290bc603116840a741c17af31a278f1
jovan-jelicki <jovanzte@gmail.com>
2021-06-08 03:08:58 +0200

    update - buildDockerfiles script

--- backend/buildDockerfiles.sh
+++ backend/buildDockerfiles.sh
@@ -1,2 +1,3 @@
 docker build -t user_service -f UserServiceDockerfile .
 docker build -t content_service -f ContentServiceDockerfile .
+docker build -t recommendation_service -f RecommendationServiceDockerfile .
