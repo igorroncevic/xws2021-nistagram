55a1983446c963d82fbfb733d5e942b4989fa47c
igorroncevic <ironcevic98@gmail.com>
2021-06-18 16:36:20 +0200

    bugfix - Fixed horizontal story scroll

--- frontend/src/components/StoryCompoent/Stories.js
+++ frontend/src/components/StoryCompoent/Stories.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import HorizontalScroll from "react-scroll-horizontal";
+import ScrollMenu from "react-horizontal-scrolling-menu";
 import Story from "./Story";
 import '../../style/Stories.css';
 import { useSelector } from 'react-redux';
@@ -13,20 +13,27 @@ const Stories = () => {
     useEffect(()=>{
         storyService.getHomepageStories({ jwt: store.user.jwt })
             .then(response => {
-                if(response.status === 200) setStories([...response.data.stories])
-                console.log(response.data.stories)
+                if(response.status === 200) {
+                    setStories(convertToComponentArray([...response.data.stories]));
+                }
             })
             .catch(err => {
                 console.log(err)
                 toastService.show("error", "Could not retrieve homepage stories.")
             })
     }, [])
+    
+    const convertToComponentArray = (stories) => stories.map((story) => <Story story={story} /> )
 
     return (
         <div className="stories">
-            {/* <HorizontalScroll className="scroll" reverseScroll={false}> */}
-                {stories.map((story) => <Story story={story} /> )}
-            {/* </HorizontalScroll> */}
+            <ScrollMenu 
+                alignCenter={false}
+                data={stories}
+                wheel={true}
+                hideArrows={true}
+                hideSingleArrow={true}
+                />
         </div>
     );
 }
