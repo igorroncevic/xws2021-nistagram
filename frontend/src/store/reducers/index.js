58103674456fda45a3ca470d8d681e8c714b5aab
David Drvar <david.drvar.bogdanovic@gmail.com>
2021-06-21 17:09:49 +0200

    fix - delete unnecessary story store

--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -2,10 +2,9 @@ import { combineReducers } from 'redux';
 
 import { user } from './user.reducer'
 import {followers} from "./followers.reducer";
-import {storyForReport} from "./storyForReport.reducer";
 
 const rootReducer = combineReducers({
-    user,followers, storyForReport
+    user,followers
 })
 
 export default rootReducer;
