2f5fd64c42ddf078b72518c01eb41ebc499e46f8
igorroncevic <ironcevic98@gmail.com>
2021-06-21 10:36:33 +0200

    bugfix - Minor bug fixes

--- frontend/src/services/favorites.service.js
+++ frontend/src/services/favorites.service.js
@@ -32,6 +32,20 @@ class FavoritesService extends RootService {
             })
         return response
     }
+
+    async getUserFavorites(data){
+        const { userId, jwt } = data;
+        const headers = this.setupHeaders(jwt);
+
+        const response = this.apiClient.get(`/${userId}`, { headers })
+            .then(res => {
+                return res
+            }).catch(err => {
+                console.error(err)
+                return err
+            })
+        return response
+    }
     
     async getUserFavoritesOptimized(data){
         const { userId, jwt } = data;
