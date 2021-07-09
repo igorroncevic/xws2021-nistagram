e8f0c5c9af8cc4fbd12a1a98a183f413fd7cd338
David Drvar <david.drvar.bogdanovic@gmail.com>
2021-07-10 00:50:16 +0200

    fix - product photo

--- frontend-agent/src/components/Product/Product.js
+++ frontend-agent/src/components/Product/Product.js
@@ -206,7 +206,8 @@ const Product = () => {
             <Navigation/>
             <div className="profileGrid">
                 <div className="card">
-                    {/*<img src={""} alt="product photo" style="width:100%"/>*/}
+                    {console.log(product)}
+                    <img src={product.photo} alt="product photo" style={{width: "50%"}}/>
                         <h1>{product.name}</h1>
 
                     {product.agentId === store.user.id && <Button style={{width: '250px'}} variant={"outline-primary"} onClick={handleEditModal}>Edit</Button>}
